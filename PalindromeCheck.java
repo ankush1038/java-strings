0071a188a0710940318b7801e0135974a7bc86b1
ankush1038 <ankusharma1029@gmail.com>
2025-03-06 14:13:42 +0530

    [Ankush] Add: Palindrome string checked

---
+++ PalindromeCheck.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        sc.close();
+
+        boolean result1 = isPalindromeIterative(input);
+        boolean result2 = isPalindromeRecursive(input, 0, input.length() - 1);
+        boolean result3 = isPalindromeUsingArrays(input);
+
+        System.out.println("\nPalindrome Check Results:");
+        System.out.println("-------------------------");
+        System.out.println("Using Iteration   : " + (result1 ? "Palindrome" : "Not a Palindrome"));
+        System.out.println("Using Recursion   : " + (result2 ? "Palindrome" : "Not a Palindrome"));
+        System.out.println("Using Char Arrays : " + (result3 ? "Palindrome" : "Not a Palindrome"));
+    }
+
+    public static boolean isPalindromeIterative(String text) {
+        int start = 0;
+        int end = text.length() - 1;
+
+        while (start < end) {
+            if (text.charAt(start) != text.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static boolean isPalindromeRecursive(String text, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (text.charAt(start) != text.charAt(end)) {
+            return false;
+        }
+        return isPalindromeRecursive(text, start + 1, end - 1);
+    }
+
+    public static boolean isPalindromeUsingArrays(String text) {
+        char[] original = text.toCharArray();
+        char[] reversed = reverseString(text);
+
+        for (int i = 0; i < original.length; i++) {
+            if (original[i] != reversed[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static char[] reverseString(String text) {
+        char[] reversed = new char[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            reversed[i] = text.charAt(text.length() - 1 - i);
+        }
+        return reversed;
+    }
+}
+
+/*
+Input : 
+Enter a string: nitin
+
+Output :
+Palindrome Check Results:
+-------------------------
+Using Iteration   : Palindrome
+Using Recursion   : Palindrome
+Using Char Arrays : Palindrome
+*/
+
